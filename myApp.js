b23475b3b6e8a15071756b709efda8118388e26c
James Martin <james.martin@helpmesee.org>
2023-12-11 22:50:14 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -25,7 +25,14 @@ app.use(helmet.dnsPrefetchControl());
 
 app.use(helmet.noCache());
 
-
+app.use(helmet.contentSecurityPolicy(
+  { directives: 
+    { 
+      defaultSrc: ["'self'"], 
+      scriptSrc: ["'self'", "trusted-cdn.com"] 
+    }
+  }
+  ));
 
 
 
